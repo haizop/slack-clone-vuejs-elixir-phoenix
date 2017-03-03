80c34d4bfc18dabdf283702a92ea6a2612fca55a
Haiz Oppenheimer <haizop@gmail.com>
2017-03-03 18:10:23 -0500

    fix

--- web/src/store/session/state.js
+++ web/src/store/session/state.js
@@ -7,9 +7,3 @@ export default {
   isAuthenticated: true,
   token: 'eyJhbGciOiJIUzUxMiIsInR5cCI6IkpXVCJ9.eyJhdWQiOiJVc2VyOjEiLCJleHAiOjE0OTExNzMxMjksImlhdCI6MTQ4ODU4MTEyOSwiaXNzIjoiU2xpbmciLCJqdGkiOiIzMTRjZGJmYS00MjEwLTRmMTktYWVkZC00NjIxYmUxZTZiZGEiLCJwZW0iOnt9LCJzdWIiOiJVc2VyOjEiLCJ0eXAiOiJhY2Nlc3MifQ.4RKxmO2F_elpy7ioc4u3X7Jo4KU2Rp9H30hyyM8OiylsSyKrp3FHF2fSIKeHKMhme834xAwBay6_NEopnVG4cQ',
 }
-// 
-// export default {
-//   currentUser: {},
-//   isAuthenticated: false,
-//   token: null,
-// }
